ae0be71bbf2f53bfe21023af9df78a79b9d5ed39
pablo <163914933+2madrugadas@users.noreply.github.com>
2026-06-21 05:33:18 -0400

    Delete js directory

--- js/grid.js
+++
@@ -1,8 +0,0 @@
-export function setupContentGrid() {
-  document.querySelectorAll('.grid-item').forEach(item => {
-    const title = item.querySelector('img')?.alt || '';
-    if (title) item.setAttribute('data-title', title);
-    const img = item.querySelector('img');
-    if (img) img.classList.add('loaded');
-  });
-}
